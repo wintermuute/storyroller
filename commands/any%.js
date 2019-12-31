3107086f524cce7cf573b935f66048c9ed05f4da
wintermuute <avasweet21@gmail.com>
2019-12-31 13:53:27 -0500

    Update any%.js

--- commands/any%.js
+++ commands/any%.js
@@ -17,7 +17,7 @@ module.exports = {
 		var totalsList = new Array(fullGames.length);
 		var ownedList = new Array(fullGames.length);
 		
-		for(var i = 0; i < totals.length; i++)
+		for(var i = 0; i < totalsList.length; i++)
 			x[i] = new Array(3);
 		
 		//Scans through booklist and counts up how many books of each game version are owned, along with the total count
