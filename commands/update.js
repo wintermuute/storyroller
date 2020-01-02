0f6e8e7bf1f9b243fd047b7b2d4034007ae0cde5
wintermuute <avasweet21@gmail.com>
2020-01-02 17:02:04 -0500

    periodic backup of booklist.json

--- commands/update.js
+++ commands/update.js
@@ -5,6 +5,6 @@ module.exports = {
 	name: 'update',
 	description: 'test',
 	execute(message, args) {
-		message.channel.send("Storyroller successfully updated to display this message!";
+		message.channel.send("Storyroller successfully updated to display this message!");
 	},
 };
